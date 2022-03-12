16816f98f3e17cbc2dbdd7aa4e87286744ab9b20
Wen Hao <haowenphoon@gmail.com>
2022-03-12 21:15:43 +0800

    Update Binding

---
+++ app/src/main/java/my/edu/tarc/ezcharge/Charging/ChargingCompleteActivity.kt
@@ -0,0 +1,11 @@
+package my.edu.tarc.ezcharge.Charging
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ChargingCompleteActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_charging_complete)
+    }
+}
