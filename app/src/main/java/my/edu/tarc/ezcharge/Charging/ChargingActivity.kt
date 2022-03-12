a708e6995559e37ce5d3ad81be5a9e65a8b9a545
Wen Hao <haowenphoon@gmail.com>
2022-03-12 19:17:49 +0800

    Update Binding

--- app/src/main/java/my/edu/tarc/ezcharge/Charging/ChargingActivity.kt
+++ app/src/main/java/my/edu/tarc/ezcharge/Charging/ChargingActivity.kt
@@ -3,10 +3,36 @@ package my.edu.tarc.ezcharge.Charging
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import my.edu.tarc.ezcharge.R
+import my.edu.tarc.ezcharge.databinding.ActivityChargingBinding
 
 class ChargingActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityChargingBinding
+    private var progr = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_charging)
+        binding = ActivityChargingBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        updateProgressBar()
+
+        binding.buttonIncr.setOnClickListener {
+            if(progr <= 90){
+                progr += 10
+                updateProgressBar()
+            }
+        }
+
+        binding.buttonDecr.setOnClickListener {
+            if(progr >= 10){
+                progr -= 10
+                updateProgressBar()
+            }
+        }
+    }
+
+    private fun updateProgressBar(){
+        binding.progressBar.progress = progr
+        binding.textViewProgress.text = "$progr%"
     }
 }
